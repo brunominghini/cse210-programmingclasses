403bd92815365ba550563780778ea43db8fe0165
brunominghini <brunoaminghini@outlook.com.br>
2023-01-12 20:35:25 -0300

    Done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        /*Console.Write("What is a magic number?");
+        string number = Console.ReadLine();*/
+        int magic_number = randomGenerator.Next(1,100);
+        int guess_number;
+        /*To test 
+        Console.WriteLine($"{magic_number}");*/
+        
+        do{
+             Console.Write("What is your guess?");
+            string answer = Console.ReadLine();
+            guess_number = int.Parse(answer);
+            if (guess_number > magic_number){
+                Console.WriteLine("Higher");
+
+            } 
+            else if(guess_number<magic_number){
+                Console.WriteLine("Lower");                
+            }
+            else if(guess_number==magic_number){
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        while (guess_number != magic_number);
+
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers_list = new List<int>();
+        
+        int answer_user = -1;
+        while (answer_user != 0)
+        {
+         Console.Write("Enter a list of numbers, type 0 to exit:");
+        
+        string answer = Console.ReadLine();
+         answer_user = int.Parse(answer);
+        
+        if(answer_user !=0){
+            numbers_list.Add(answer_user);
+        }
+        }
+        int sum = 0;
+        foreach(int number in numbers_list){
+            sum = sum+number;
+            //Console.WriteLine(numbers_list[number]);
+        }
+        Console.WriteLine($"The sum is:{sum}");
+
+        float average = ((float)sum)/numbers_list.Count;
+        Console.WriteLine($"The Average is:{Math.Round(average,2)}");
+
+        int highest = numbers_list[0];
+
+        for (int i = 0; i < numbers_list.Count; i++)
+        {
+            int number = numbers_list[i];
+            if (number > highest)
+            {
+                highest = number;
+            }
+        }
+        Console.WriteLine($"The Highest is:{highest}");
     }
+    
 }
