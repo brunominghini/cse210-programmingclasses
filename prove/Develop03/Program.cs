873d357feeccc69cbfd18e7587697927dd9739cb
brunominghini <brunoaminghini@outlook.com.br>
2023-02-14 21:40:54 -0300

    Modified

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,7 +3,10 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {   
+    {   ShowingScripture randomScripture = new ShowingScripture();
+        
+        
+        string option = "";
         Scripture scripture = new Scripture();
         Reference reference = new Reference();
 
@@ -13,8 +16,16 @@ class Program
         Console.WriteLine("What reference?");
         reference.SetReference(Console.ReadLine());
 
+        Console.WriteLine ("to press the enter key or type quit:");
+        option = Console.ReadLine();
+
+        do
+        {
+            
+        }
+        while(option == "quit");
 
         Console.WriteLine(reference.GetReference()+": "+scripture.GetScripture());
-
+        
     }
 }
