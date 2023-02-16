67237e5a90e297383e63290227360f59bcc43d6f
brunominghini <brunoaminghini@outlook.com.br>
2023-02-15 23:40:14 -0300

    Modified

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assigment = new Assignment("Bruno Minghini","Multiplication");
+        Console.WriteLine(assigment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Bruno Minghini", "Calc", "2.0", "10");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Angela Pedreira", "Brazil History", "The Colonization");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
+
     }
 }
