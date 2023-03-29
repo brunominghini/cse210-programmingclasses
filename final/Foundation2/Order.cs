a0d9b18c5f7ac94fabe9b1653687f65bccd3a14b
brunominghini <brunoaminghini@outlook.com.br>
2023-03-28 22:47:04 -0300

    "Modified"

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -1,18 +1,40 @@
-public class Order{
+public class Order:Product
+{
 
     private double totalPrice;
-
     List<Product> listOrder = new List<Product>();
     Customer customer = new Customer();
 
 
-    public void setTotalPrice(){
-        
+    public void setProductOrder()
+    {
+    Console.Write (" Whats name of Product: ");
+    string nameProduct = Console.ReadLine();
+    setName(nameProduct);
+
+    Console.Write ("How price unit of Product: ");
+    double priceU = Convert.ToInt32(Console.ReadLine());
+    setPriceU(priceU);
+
+    Console.Write ("How many quantity of Product: ");
+    int IdProduct = Convert.ToInt32(Console.ReadLine());
+
+    double totalPrice = GetPrice();
+    }
+   
+    public string getPackingLabel(){
+        string nameProduct = getName();
+        string parckingLabel ="ID: " + nameProduct;
+
+        return parckingLabel;
     }
 
-   
+    public string getShippingLabel()
+    {
 
+        string shippingLabel = getName() + customer.GetFullAddress();
 
-
+        return shippingLabel;
+    }
 
 }
